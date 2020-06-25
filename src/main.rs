5613a14a1f5980df0a3b2cb3ee7748ae4201d48a
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-06-25 23:10:40 +0100

    Add basic get-entries call

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Eq, PartialEq)]
 struct Logs {
     entries: Vec<LogEntry>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Eq, PartialEq)]
 struct LogEntry {
     /// The `leaf_input` field is a `String` of base64 encoded data. The data is a DER encoded
     /// MerkleTreeHeader, which has the following structure.
@@ -29,39 +29,78 @@ struct LogEntry {
     extra_data: String,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    Ok(())
+async fn get_entries_from(start: usize) -> Result<Logs, Box<dyn std::error::Error>> {
+    let response = reqwest::get(&format!(
+        "http://localhost:5000/logs/argon2020/ct/v1/get-entries?start={}&end={}",
+        start,
+        start + 31
+    ))
+    .await?;
+    if response.status() != 200 {
+        return Err("An error occurred retrieving logs".into());
+    }
+    let logs = response.json::<Logs>().await?;
+    Ok(logs)
 }
+
+fn main() {}
 
 #[cfg(test)]
 mod test {
-    use crate::LogEntry;
+    use crate::{get_entries_from, LogEntry, Logs};
     use httpmock::Method::GET;
-    use httpmock::{mock, with_mock_server, Mock};
-    use reqwest;
+    use httpmock::{mock, with_mock_server};
     use tokio;
 
     const LEAF_INPUT: &str = include_str!("../resources/leaf_input_with_cert");
 
     #[tokio::test]
     #[with_mock_server]
-    async fn should_call_argon_logs() {
-        let logs_mock = mock(GET, "/logs/argon2020/ct/v1/get-entries")
-            .expect_query_param("start", "1")
-            .expect_query_param("end", "2")
-            .return_json_body(&LogEntry {
-                leaf_input: LEAF_INPUT.to_owned(),
-                extra_data: "".to_owned(),
-            })
+    async fn should_fail_if_log_retrieval_fails() {
+        let _ = mock(GET, "/logs/argon2020/ct/v1/get-entries")
+            .expect_query_param("start", "0")
+            .expect_query_param("end", "31")
+            .return_status(400)
+            .create();
+        let result = get_entries_from(0).await;
+        assert!(result.is_err());
+        assert_eq!(
+            format!("{}", result.err().unwrap()),
+            "An error occurred retrieving logs"
+        );
+    }
+
+    #[tokio::test]
+    #[with_mock_server]
+    async fn should_fail_if_body_is_not_an_expected_value() {
+        let body = vec![0, 0];
+        let _ = mock(GET, "/logs/argon2020/ct/v1/get-entries")
+            .expect_query_param("start", "0")
+            .expect_query_param("end", "31")
+            .return_json_body(&body)
             .return_status(200)
             .create();
+        let result = get_entries_from(0).await;
+        assert!(result.is_err());
+    }
 
-        let response = reqwest::get(&format!(
-            "http://localhost:5000/logs/argon2020/ct/v1/get-entries?start={}&end={}",
-            1, 2
-        ))
-        .await
-        .expect("failed to get");
-        assert_eq!(response.status(), 200)
+    #[tokio::test]
+    #[with_mock_server]
+    async fn should_return_logs() {
+        let body = Logs {
+            entries: vec![LogEntry {
+                leaf_input: LEAF_INPUT.to_owned(),
+                extra_data: "".to_owned(),
+            }],
+        };
+        let _ = mock(GET, "/logs/argon2020/ct/v1/get-entries")
+            .expect_query_param("start", "0")
+            .expect_query_param("end", "31")
+            .return_json_body(&body)
+            .return_status(200)
+            .create();
+        let result = get_entries_from(0).await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), body);
     }
 }
