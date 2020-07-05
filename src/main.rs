15c5c76392f6c0bbfb8fbe172e70efbe1035e857
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-07-05 17:19:23 +0100

    Move consumer to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 mod client;
-use async_compression::tokio_02::write::GzipEncoder;
+mod consumer;
+
 use client::{CtClient, HttpCtClient, Logs};
+use consumer::Consumer;
 use futures::{stream::FuturesUnordered, StreamExt};
-use std::{
-    error::Error,
-    path::{Path, PathBuf},
-    sync::Arc,
-};
-use tokio::io::AsyncWriteExt;
+use std::{error::Error, sync::Arc};
 use tokio::signal::unix::{signal, SignalKind};
-use tokio::{fs::OpenOptions, join, sync::mpsc, sync::oneshot};
+use tokio::{join, sync::mpsc, sync::oneshot};
 
 const RETRIEVAL_LIMIT: usize = 32;
 const CT_LOGS_URL: &str = "https://ct.googleapis.com/aviator/ct/v1";
@@ -18,7 +15,7 @@ const CT_LOGS_URL: &str = "https://ct.googleapis.com/aviator/ct/v1";
 async fn main() -> Result<(), Box<dyn Error>> {
     let (sigint_tx, sigint_rx) = oneshot::channel();
     let client = Box::new(HttpCtClient::new(CT_LOGS_URL));
-    let (tx, rx) = mpsc::channel(100);
+    let (tx, rx) = mpsc::channel(2000);
     let (producer_result, consumer_result, sigint_result) = join!(
         Producer::new(client, tx, sigint_rx).produce(),
         Consumer::new(rx).consume("."),
@@ -67,7 +64,7 @@ impl Producer {
                 let logs = c.get_entries(start, end).await?;
                 Ok::<Logs, Box<dyn Error>>(logs)
             });
-            if queue.len() == 12 {
+            if queue.len() == 48 {
                 while let Some(Ok(logs)) = queue.next().await {
                     self.logs_tx.send(logs).await?;
                 }
@@ -87,36 +84,6 @@ impl Producer {
     }
 }
 
-struct Consumer {
-    logs_rx: mpsc::Receiver<Logs>,
-}
-
-impl Consumer {
-    fn new(logs_rx: mpsc::Receiver<Logs>) -> Self {
-        Self { logs_rx }
-    }
-
-    async fn consume<P: AsRef<Path>>(mut self, path: P) -> Result<(), Box<dyn Error>> {
-        let mut path = PathBuf::from(path.as_ref());
-        path.push("logs.gz");
-        let writer = OpenOptions::new()
-            .create(true)
-            .append(true)
-            .write(true)
-            .open(&path)
-            .await?;
-        let mut gzip = GzipEncoder::new(writer);
-        while let Some(logs) = self.logs_rx.recv().await {
-            for entry in logs.entries {
-                gzip.write_all(entry.leaf_input.as_bytes()).await?;
-                gzip.write_all(b"\n").await?;
-            }
-        }
-        gzip.shutdown().await?;
-        Ok(())
-    }
-}
-
 async fn signal_handler(signal_tx: oneshot::Sender<()>) -> Result<(), Box<dyn Error>> {
     let mut sigint = signal(SignalKind::interrupt())?;
     sigint.recv().await;
@@ -131,15 +98,11 @@ async fn signal_handler(signal_tx: oneshot::Sender<()>) -> Result<(), Box<dyn Er
 mod test {
     use crate::{
         client::{CtClient, LogEntry, Logs},
-        Consumer, Producer,
+        Producer,
     };
-    use async_compression::tokio_02::bufread::GzipDecoder;
     use async_trait::async_trait;
-    use tokio::io::AsyncReadExt;
     use tokio::{
         self,
-        fs::OpenOptions,
-        io::BufReader,
         sync::{mpsc, oneshot},
         time::{timeout_at, Duration, Instant},
     };
@@ -170,34 +133,6 @@ mod test {
         async fn get_tree_size(&self) -> Result<usize, Box<dyn std::error::Error>> {
             Ok(self.tree_size)
         }
-    }
-
-    #[tokio::test]
-    async fn consumer_should_write_items_received_on_channel_compressed_and_separated_with_a_newline(
-    ) {
-        let expected_data = "test";
-        let dir = tempfile::tempdir().unwrap();
-        let path = dir.into_path();
-        let p = path.clone();
-        let (mut tx, rx) = mpsc::channel(100);
-        tx.send(Logs {
-            entries: vec![LogEntry {
-                leaf_input: expected_data.to_owned(),
-                extra_data: "".to_owned(),
-            }],
-        })
-        .await
-        .unwrap();
-        let handle = tokio::spawn(async move { Consumer::new(rx).consume(&p).await.unwrap() });
-        drop(tx);
-        handle.await.unwrap();
-        let mut path = path.to_owned();
-        path.push("logs.gz");
-        let file = OpenOptions::new().read(true).open(&path).await.unwrap();
-        let mut decoder = GzipDecoder::new(BufReader::new(file));
-        let mut actual = String::new();
-        decoder.read_to_string(&mut actual).await.unwrap();
-        assert_eq!(actual, format!("{}\n", expected_data));
     }
 
     #[tokio::test]
