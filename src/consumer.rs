679265468b172f6b846e01b7b1e7cf4543745768
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-07-06 21:35:20 +0100

    Add extraction for san

--- src/consumer.rs
+++ src/consumer.rs
@@ -1,15 +1,20 @@
 use crate::client::Logs;
 use async_compression::tokio_02::write::GzipEncoder;
+use der_parser::ber::BerObjectContent;
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 use tokio::io::{AsyncWrite, AsyncWriteExt};
 use tokio::sync::mpsc;
-use x509_parser::TbsCertificate;
+use x509_parser::{
+    objects::{oid2nid, Nid},
+    TbsCertificate,
+};
 
 #[derive(Default, Deserialize, Serialize)]
 struct CertInfo {
     pub issuer: String,
     pub subject: String,
+    pub san: Vec<String>,
 }
 
 pub struct Consumer {
@@ -38,7 +43,7 @@ impl Consumer {
                     let length = u32::from_be_bytes([0, bytes[12], bytes[13], bytes[14]]);
                     let end = length as usize + start;
                     let (_, cert) = x509_parser::parse_x509_der(&bytes[start..end])?;
-                    let info = extract_cert_info(cert.tbs_certificate);
+                    let info = extract_cert_info(cert.tbs_certificate)?;
                     let bytes = serde_json::to_vec(&info)?;
                     gzip.write(&bytes).await?;
                 }
@@ -49,12 +54,28 @@ impl Consumer {
     }
 }
 
-fn extract_cert_info(cert: TbsCertificate) -> CertInfo {
-    let info = CertInfo {
-        issuer: cert.issuer.to_string(),
-        subject: cert.subject.to_string(),
-    };
-    info
+fn extract_cert_info(cert: TbsCertificate) -> Result<CertInfo, Box<dyn Error>> {
+    let issuer = cert.issuer;
+    let subject = cert.subject;
+    let mut san = Vec::new();
+    for extension in cert.extensions {
+        if let Ok(Nid::SubjectAltName) = oid2nid(&extension.oid) {
+            let (_, obj) = der_parser::parse_der(extension.value)?;
+            obj.as_sequence()?
+                .into_iter()
+                .for_each(|item| match item.content {
+                    BerObjectContent::Unknown(_, bytes) => {
+                        san.push(String::from_utf8_lossy(bytes).to_string());
+                    }
+                    _ => println!("Failed to read subject alternative name: {:?}", obj),
+                });
+        }
+    }
+    Ok(CertInfo {
+        issuer: issuer.to_string(),
+        subject: subject.to_string(),
+        san,
+    })
 }
 
 #[cfg(test)]
@@ -68,6 +89,17 @@ mod test {
         io::{AsyncReadExt, BufReader},
         sync::mpsc,
     };
+
+    #[tokio::test]
+    async fn consume_should_extract_subject_alternative_names() {
+        let leaf_input = include_str!("../resources/leaf_input_with_cert").trim();
+        let mut consumer = init_consumer_with(leaf_input).await;
+        let mut buf: Vec<u8> = Vec::new();
+        let result = consumer.consume(Cursor::new(&mut buf)).await;
+        let info = serde_json::from_slice::<CertInfo>(&decode(&buf).await.unwrap()).unwrap();
+        assert!(result.is_ok());
+        assert_eq!(info.san, vec!["www.libraryav.com.au", "libraryav.com.au"]);
+    }
 
     #[tokio::test]
     async fn consume_should_skip_precerts() {
